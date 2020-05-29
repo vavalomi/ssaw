525899b3a5f13b49121a208a5b74dbc0642067d6
Zurab Sajaia <16375540+vavalomi@users.noreply.github.com>
2020-05-29 17:22:20 -0400

    run tests against recorded cassettes

--- tests/headquarters/__init__.py
+++ tests/headquarters/__init__.py
@@ -5,6 +5,6 @@ my_vcr = vcr.VCR(
 	serializer='yaml',
 	cassette_library_dir='tests/headquarters/vcr_cassettes',
 	path_transformer=vcr.VCR.ensure_suffix('.yaml'),
-	record_mode='once',
+	record_mode='none',
 	filter_headers=[('authorization', None)]
 )
